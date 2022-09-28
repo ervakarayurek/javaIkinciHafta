ecc777fe19e3f1f38b4c3908099d03c0687881fe
Erva Karayürek <73640110+ervakarayurek@users.noreply.github.com>
2022-09-28 18:10:19 +0300

    Add files via upload

---
+++ inheritanceDemo/src/inheritanceDemo/BaseKrediManager.java
@@ -0,0 +1,8 @@
+package inheritanceDemo;
+
+public class BaseKrediManager {
+	//Bir class yalnızca bir tane class'ı extend edebilir.
+	public void Hesapla() {
+		System.out.println("Kredi hesaplandı.");
+	}
+}
---
+++ inheritanceDemo/src/inheritanceDemo/TarimKrediManager.java
@@ -0,0 +1,10 @@
+package inheritanceDemo;
+
+public class TarimKrediManager extends BaseKrediManager{
+	
+}
+
+
+
+
+
